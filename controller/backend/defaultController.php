6d426baf1a5f3b6982841511238300136cd1a615
turtec <turowski@turtec.de>
2013-05-11 15:36:05 +0200

    Changeing folderstructure views

--- controller/backend/defaultController.php
+++ controller/backend/defaultController.php
@@ -32,7 +32,7 @@ class defaultController {
          * Default Callback-Method 
          */
         public function aCallbackMethod(){
-            $thePath='/../../viewsbackend/default.php';
+            $thePath='/../../views/backend/default.php';
             HTMLRenderer::renderHTML($thePath, $params);
         }    
         
