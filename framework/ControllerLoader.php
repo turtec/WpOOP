00faa48926e7a20f620b5a351a559deb6594ee32
turtec <turowski@turtec.de>
2013-05-11 11:33:12 +0200

    Documentaion extended

--- framework/ControllerLoader.php
+++ framework/ControllerLoader.php
@@ -2,7 +2,10 @@
 /**
  * Loads all Controller-Classes in a given Path
  * Classname has to be the same like the Filename
- *
+ * This Class automatical loads all Controller
+ * in the controller-Folders
+ * so u don't have to include and load these
+ * classes manually
  * @author Sascha Turowski <turtec.de>
  */
 class ControllerLoader {
@@ -15,11 +18,13 @@ class ControllerLoader {
          */
         public static function loadControllers($path,$dir){
            foreach( glob($dir.'/'. $path .'/*.php' ) as $controllerPath ) {
+                //include the File
                 include $controllerPath;
                 //get the filename form path
                 $fileName = basename($controllerPath);
                 //get the name without extentions
                 $parts=explode('.', $fileName);
+                //get the classname by accessing the array
                 $className=$parts[0]; 
                 //load the class
                 $aCtl = new $className(); 
